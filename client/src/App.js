200cfceafc73b4ca90189b43647c363e30f8776b
Naa Norkor <nortey.sarah@gmail.com>
2022-10-28 13:15:50 +0100

    profile page styling part II

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,16 @@ import Sidebar from "./Components/Sidebar";
 import "./styles/partials/_resets.scss";
 import Profile from "./pages/Profile/Profile";
 import "./App.scss";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <section className="wrapper">
-        <div className="sidebar">
+        <div className="sidebar-component">
           <Sidebar />
         </div>
-        <div className="profile">
+        <div className="allroutes">
           <Routes>
             <Route path="/" element={<Profile />}></Route>
             <Route path="/dashboard" element={<Dashboard />}></Route>
